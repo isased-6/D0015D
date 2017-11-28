7316c646c85086bf1cba6fb75498cf0dbccfbb76
isased-6 <isased-6@student.ltu.se>
2017-11-28 17:02:46 +0100

    Fancybox

---
+++ javascripts/fancybox.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+    $(".bildvisning").fancybox({
+        'transitionIn'	:	'elastic',
+        'transitionOut'	:	'elastic',
+        'speedIn'		:	600, 
+        'speedOut'		:	200, 
+        'overlayShow'	:	false
+    });
+    
+    $(".bildvisning").fancybox();
+});
--- javascripts/script.js
+++ javascripts/script.js
@@ -88,17 +88,3 @@
   /*
   END Slider
   */
-
-
-
-$( document ).ready(function() {
-    $(".bildvisning").fancybox({
-        'transitionIn'	:	'elastic',
-        'transitionOut'	:	'elastic',
-        'speedIn'		:	600, 
-        'speedOut'		:	200, 
-        'overlayShow'	:	false
-    });
-    
-    $(".bildvisning").fancybox();
-});
